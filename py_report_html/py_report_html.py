984209eccc629b567751fed5a2b580486ea3630e
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2023-04-18 15:14:56 +0200

    Added buttons for copy, csv, excel, pdf and print with DataTables

--- py_report_html/py_report_html.py
+++ py_report_html/py_report_html.py
@@ -74,9 +74,18 @@ class Py_report_html:
 
         if len(self.dt_tables) > 0: # CDN load, this library is difficult to embed in html file
             self.all_report += '<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap.min.css"/>'+"\n"
+            self.all_report += '<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"/>'+"\n"
+            self.all_report += '<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css"/>'+"\n"
             self.all_report += '<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>' + "\n"
             self.all_report += '<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>' + "\n"
             self.all_report += '<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap.min.js"></script>' + "\n"
+            self.all_report += '<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>' + "\n"
+            self.all_report += '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>' + "\n"
+            self.all_report += '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>' + "\n"
+            self.all_report += '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>' + "\n"
+            self.all_report += '<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>' + "\n"
+            self.all_report += '<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>' + "\n"
+
 
         if 'sigma' in self.networks: # sigma CDN load is HUGE so we read it from file
             with open(os.path.join(Py_report_html.TEMPLATES, 'sigma_cdn.txt'), 'r') as f:
@@ -122,7 +131,7 @@ class Py_report_html:
                 f"<script>\n"
                 f"    % for dt_table in plotter.dt_tables:\n"
                 f"        $(document).ready(function () {{\n"
-                f"            $('#${{ dt_table }}').DataTable();\n"
+                f"            $('#${{ dt_table }}').DataTable({{ dom:'Bfrtip', buttons: ['copy', 'csv', 'excel', 'pdf', 'print'] }});\n"
                 f"        }});\n"
                 f"    % endfor\n"
                 f"</script>\n")
